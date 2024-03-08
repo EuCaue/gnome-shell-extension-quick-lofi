1f78abda04fd7c14de2e98426bdb215f434a1fc9
EuCaue <caueadriano.castro@gmail.com>
2024-03-08 09:06:47 -0300

    feat(extension): play/pause radios && use radios from gschema
    Play pause radios now works as expected, using radios from GSchema, instead of a JSON file.

--- src/extension.ts
+++ src/extension.ts
@@ -6,37 +6,51 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
-import { ICONS, RADIOS, UUID } from './consts';
+import { ICONS, UUID } from './consts';
 import { Player } from './Player';
 let activeChild = null;
+type Radio = { radioName: string; radioUrl: string };
 
 class Indicator extends PanelMenu.Button {
   static {
     GObject.registerClass(this);
   }
   private mpvPlayer = new Player();
+  private _radios?: Array<Radio>;
+  private _settings: Gio.Settings;
+
+  private _createRadiosArray(): void {
+    const radios: string[] = Extension.lookupByUUID(UUID).getSettings().get_strv('radios');
+    radios.forEach((entry: string) => {
+      const [radioName, radioUrl] = entry.split(' - ');
+      this._radios.push({ radioName, radioUrl });
+    });
+  }
+
   constructor() {
     super(0.0, 'Quick Lofi');
     this.mpvPlayer = new Player();
     this.mpvPlayer.init();
+    this._radios = [];
+    this._settings = Extension.lookupByUUID(UUID).getSettings();
+    this._settings.connect('changed', (_, key) => {
+      if (key === 'radios') {
+        this._updateMenuItems();
+      }
+    });
+    this._createRadiosArray();
   }
 
   private _togglePlayingStatus(child: PopupMenu.PopupImageMenuItem) {
-    const currentRadioUrl = RADIOS.find((radio) => radio.radioName === child.label.text).radioUrl;
+    // console.log('radios find >>>>>>>>>>', this._radios);
+    const currentRadioUrl = this._radios.find((radio) => radio.radioName === child.label.text).radioUrl;
 
     if (child === activeChild) {
-      //  TODO: maybe find a diff icon for the last played child
-
-      if (this.mpvPlayer.isPlaying) {
-        this.mpvPlayer.pause();
-        activeChild.setIcon(ICONS.PLAY);
-      } else {
-        this.mpvPlayer.resume();
-        activeChild.setIcon(ICONS.PAUSE);
-      }
+      this.mpvPlayer.stopPlayer();
+      activeChild.setIcon(ICONS.PLAY);
+      activeChild = null;
     } else {
       if (activeChild) {
-        this.mpvPlayer.stopPlayer();
         activeChild.setIcon(ICONS.PLAY);
       }
 
@@ -56,11 +70,19 @@ class Indicator extends PanelMenu.Button {
       }
     });
   }
+
+  private _updateMenuItems(): void {
+    this.menu.box.remove_all_children();
+    this._radios = [];
+    this._createRadiosArray();
+    this._createMenuItems();
+  }
+
   private _createMenuItems() {
     const scrollView = new St.ScrollView();
     const section1 = new PopupMenu.PopupMenuSection();
     scrollView.add_actor(section1.actor);
-    RADIOS.forEach((radio) => {
+    this._radios.forEach((radio) => {
       const menuItem = new PopupMenu.PopupImageMenuItem(radio.radioName, ICONS.PLAY);
       menuItem.connect('activate', this._togglePlayingStatus.bind(this));
       section1.addMenuItem(menuItem);
@@ -72,6 +94,8 @@ class Indicator extends PanelMenu.Button {
   }
 
   _init() {
+    this._radios = [];
+    this._createRadiosArray();
     super._init(0.0, 'Quick Lofi');
     const path = Extension.lookupByUUID(UUID).path;
     const gicon = Gio.icon_new_for_string(path + '/icon-symbolic.svg');
@@ -89,9 +113,15 @@ class Indicator extends PanelMenu.Button {
 
 export default class QuickLofi extends Extension {
   _indicator: Indicator = null;
+  _settings: Gio.Settings = null;
 
   enable() {
+    this._settings = this.getSettings();
     this._indicator = new Indicator();
+
+    // this._settings.connect('changed', () => {
+    //   console.log('volume changed');
+    // });
     Main.panel.addToStatusArea(this.uuid, this._indicator);
   }
 
