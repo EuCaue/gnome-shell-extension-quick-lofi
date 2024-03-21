3bced520be794fed5ad40164bf0631a6e0a8d305
EuCaue <caueadriano.castro@gmail.com>
2024-03-21 13:00:46 -0300

    feat(Utils): getExtension, getSettings, debug utils methods

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,33 +1,18 @@
+import { Extension } from '@girs/gnome-shell/extensions/extension';
 import Gio from 'gi://Gio';
 
 export default class Utils {
-  public static tempFile: Gio.File | null = null;
-  public static readonly tempFilePath: string = '/tmp/current-radio-playing';
+  public static readonly UUID = 'quick-lofi@eucaue';
 
-
-  public static deleteTempFile(): void {
-    if (this.tempFile) {
-      this.tempFile.delete(null);
-      this.tempFile = null;
-    }
+  public static getExtension(): Extension {
+    return Extension.lookupByUUID(this.UUID);
   }
 
-  public static createTempFile(currentRadioName: string): void {
-    this.tempFile = Gio.File.new_for_uri(`file://${this.tempFilePath}`);
-    const outputStream: Gio.FileOutputStream = this.tempFile.create(Gio.FileCreateFlags.NONE, null);
-    const data: Uint8Array = new TextEncoder().encode(currentRadioName);
-    outputStream.write(data, null);
-    outputStream.close(null);
+  public static getSettings(): Gio.Settings {
+    return this.getExtension().getSettings();
   }
 
-  public static readTempFile(): string | null {
-    try {
-      const tempFile: Gio.File = Gio.File.new_for_uri(`file://${this.tempFilePath}`);
-      const [_, contents] = tempFile.load_contents(null);
-      const currentRadioPlaying: string = new TextDecoder().decode(contents);
-      return currentRadioPlaying.trim();
-    } catch (error) {
-      return null;
-    }
+  public static debug(...message: any[]): void {
+    console.log('[ QUICK LOFI DEBUG ] >>> ', ...message);
   }
 }
