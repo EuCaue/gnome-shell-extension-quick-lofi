e67668af04f29c370ea24f33fd0095c634877b94
EuCaue <caueadriano.castro@gmail.com>
2024-04-14 21:09:37 -0300

    refactor(Utils): remove getExtension, getSettings and UUID.
    This will not be need anymore.

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,23 +1,10 @@
-import { Extension } from '@girs/gnome-shell/extensions/extension';
-import Gio from 'gi://Gio';
-
 export default class Utils {
-  public static readonly UUID = 'quick-lofi@eucaue';
-
   public static readonly ICONS = {
     INDICATOR_DEFAULT: '/icon-symbolic.svg',
     INDICATOR_PLAYING: '/icon-playing-symbolic.svg',
     POPUP_PLAY: 'media-playback-start-symbolic',
     POPUP_PAUSE: 'media-playback-stop-symbolic',
   };
-
-  public static getExtension(): Extension {
-    return Extension.lookupByUUID(this.UUID);
-  }
-
-  public static getSettings(): Gio.Settings {
-    return this.getExtension().getSettings();
-  }
 
   public static debug(...message: any[]): void {
     console.log('[ QUICK LOFI DEBUG ] >>> ', ...message);
