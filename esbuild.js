c4549120e3c4a7d61b4364c40cecf4949bc9f476
EuCaue <caueadriano.castro@gmail.com>
2024-07-28 14:03:53 -0300

    chore(esbuild.js): refactor build script for better readability and maintainability

--- esbuild.js
+++ esbuild.js
@@ -7,35 +7,35 @@ import metadata from './src/metadata.json' assert { type: 'json' };
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-console.debug(`Building ${metadata.name} v${metadata.version}...`);
+async function main() {
+  console.debug(`Building ${metadata.name} v${metadata.version}...`);
 
-build({
-  entryPoints: ['src/extension.ts', 'src/prefs.ts'],
-  outdir: 'dist',
-  bundle: true,
-  treeShaking: false,
-  target: 'firefox78',
-  platform: 'node',
-  format: 'esm',
-  external: ['gi://*', 'resource://*', 'system', 'gettext', 'cairo'],
-}).then(() => {
-  const iconSrc = resolve(__dirname, 'icon-symbolic.svg');
-  const iconDist = resolve(__dirname, 'dist/icon-symbolic.svg');
-  const iconPlayingSrc = resolve(__dirname, 'icon-playing-symbolic.svg');
-  const iconPlayingDist = resolve(__dirname, 'dist/icon-playing-symbolic.svg');
-  const metaSrc = resolve(__dirname, 'src/metadata.json');
-  const metaDist = resolve(__dirname, 'dist/metadata.json');
-  const stylesheetSrc = resolve(__dirname, 'src/stylesheet.css');
-  const stylesheetDist = resolve(__dirname, 'dist/stylesheet.css');
-  const schemaSrc = resolve(__dirname, 'schemas/');
-  const schemaDist = resolve(__dirname, 'dist/schemas/');
+  await build({
+    entryPoints: ['src/extension.ts', 'src/prefs.ts'],
+    outdir: 'dist',
+    bundle: true,
+    treeShaking: false,
+    target: 'firefox78',
+    platform: 'node',
+    format: 'esm',
+    external: ['gi://*', 'resource://*', 'system', 'gettext', 'cairo'],
+  });
+
+  const filesToCopy = [
+    { src: 'icon-symbolic.svg', dist: 'dist/icon-symbolic.svg' },
+    { src: 'icon-playing-symbolic.svg', dist: 'dist/icon-playing-symbolic.svg' },
+    { src: 'src/metadata.json', dist: 'dist/metadata.json' },
+    { src: 'src/stylesheet.css', dist: 'dist/stylesheet.css' },
+  ];
+
+  filesToCopy.forEach(({ src, dist }) => {
+    copyFileSync(resolve(__dirname, src), resolve(__dirname, dist));
+  });
+
+  cpSync(resolve(__dirname, 'schemas/'), resolve(__dirname, 'dist/schemas/'), { recursive: true });
+
   const zipFilename = `${metadata.uuid}.zip`;
   const zipDist = resolve(__dirname, zipFilename);
-  copyFileSync(metaSrc, metaDist);
-  copyFileSync(stylesheetSrc, stylesheetDist);
-  copyFileSync(iconSrc, iconDist);
-  copyFileSync(iconPlayingSrc, iconPlayingDist);
-  cpSync(schemaSrc, schemaDist, { recursive: true });
 
   const zip = new AdmZip();
   zip.addLocalFolder(resolve(__dirname, 'dist'));
@@ -52,4 +52,9 @@ build({
   console.log('To check if the extension has been recognized, you can execute the following: gnome-extensions list.');
   console.log(`If ${metadata.uuid} is listed in the output, you should be able to activate the extension.`);
   console.log('Otherwise, you will need to restart the GNOME Shell.');
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
